85fbc23caf8f6e543336859a12a66c45559a1fb6
Oleg Trofimov <trofimlego@MacBook-Air-Oleg.local>
2023-10-06 22:41:03 +0200

    Added Inlines in Admin

--- moscow_places/admin.py
+++ moscow_places/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from .models import Photo, Place
 
-# @admin.register(Place)
-admin.site.register(Place)
-# @admin.register(Photo)
 admin.site.register(Photo)
+
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline]
